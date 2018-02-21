86e6d323d737ebaf84d5999738f5059c8c2239fd
mahoyen <32592450+mahoyen@users.noreply.github.com>
2018-02-21 10:09:40 +0100

    removed testtext

--- makeGCode.py
+++ makeGCode.py
@@ -1,10 +1,8 @@
-import datetime
+import datetime as dt
+import py-midi
 
 gcode_file = open("test.gcode", "w")
 
-gcode_file.write("Hello World") 
-gcode_file.write("This is our new text file") 
-gcode_file.write("and this is another line.") 
-gcode_file.write("Why? Because we can.") 
- 
+gcode_file.write(";"+str(dt.datetime.now())+"\n") 
+
 gcode_file.close() 
