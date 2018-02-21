936af094bdd0723eb862c0399fde6109eebf6bae
mahoyen <32592450+mahoyen@users.noreply.github.com>
2018-02-21 17:33:47 +0100

    Added filewrite from midi file

--- makeGCode.py
+++ makeGCode.py
@@ -1,8 +1,18 @@
 import datetime as dt
-import py-midi
+import mido
 
-gcode_file = open("test.gcode", "w")
 
-gcode_file.write(";"+str(dt.datetime.now())+"\n") 
 
-gcode_file.close() 
+miditextfile = open("test.csv", "w")
+mid = mido.MidiFile("MarbleMachineRightHand.mid")
+miditextfile.write(str(dt.datetime.now())+"\n")
+
+for i, track in enumerate(mid.tracks):
+    print('Track {}: {}'.format(i, track.name))
+    for msg in track:
+        miditextfile.write(str(msg) + ";")
+        print(msg)
+
+Hei = enumerate(mid.tracks)
+print(Hei)
+miditextfile.close() 
