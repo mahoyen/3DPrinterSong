257b5750b14d145eb6e0050e19049f1230974e46
mahoyen <32592450+mahoyen@users.noreply.github.com>
2018-02-22 14:33:32 +0100

    created new function calculateNewPosition and adde BUILD_AREA constant array

--- midiToGCode.py
+++ midiToGCode.py
@@ -5,6 +5,7 @@ from mido import MidiFile
 MIDIFILENAME = "sekritstuffboth.mid"
 FREQUENCYTOFEEDRATECONSTANT = 1
 TIMETODISTANCECONSTANT = 1
+BUILDING_AREA = [223, 223, 305]
 
 # Converts from miditoneNumber to frequency
 def frequencyFromMidiNote(note):
@@ -17,6 +18,31 @@ def getFrequencyTimeVector(filename):
 # Converts frequency and time into feedrate and distance
 def getFeedrateDistanceVector(frequencyTimeVector):
     return [[FREQUENCYTOFEEDRATECONSTANT*f, FREQUENCYTOFEEDRATECONSTANT*TIMETODISTANCECONSTANT*f/T] for f, T in frequencyTimeVector]
+
+
+# Takes inn feedrate, old coordinates and relative coordinates and outputs new coordinate
+def calculateNewPosition(feedrate, oldCoordinates[3], relCoordinates[3]) {
+    
+    
+    newCoordinates = [oldCoordinates[0],oldCoordinates[1],oldCoordinates[2]]
+
+    for i in range(3):
+        if not(oldCoordinates[i] >= 0 and oldCoordinates < BUILDING_AREA[i]):
+            raise Exception("Position outside of build area")
+        if not(relCoordinates[i] >= 0 and relCoordinates < BUILDING_AREA[i])
+            raise Exception("Movement larger than build area or negativ")
+        
+        newCoordinates[i] = oldCoordinates[i] + relCoordinates[i]
+        if newCoordinates[i] >= BUILDING_AREA[i]:
+            newCoordinates[i] = oldCoordinates[i] - relCoordinates[i]
+            if newCoordinates[i] < 0:
+                raise Exception("New position is outside of build area")
+        
+
+    
+
+    return newCoordinates[]
+}
 
 # Generates gCode from feedrate and distance and saves it in filename
 def generateGCode(feedrateDistancevector, filename):
