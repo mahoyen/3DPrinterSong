51aa11f8bc109afc58d87a22081d5e1ad064408e
mahoyen <32592450+mahoyen@users.noreply.github.com>
2018-03-06 09:12:00 +0100

    Removed comment

--- MidiGcode/midi.py
+++ MidiGcode/midi.py
@@ -8,18 +8,6 @@ def frequencyFromMidiNote(note):
 
 # Returns all the frequencies and durations from all the tone in filename
 def getFrequencyTimeMatrix(filename):
-    '''
-    frequencyTimeMatrix = list()
-    i = 0
-    for msg in MidiFile(filename).tracks[0]:
-        if (msg.type == "note_on"):
-            frequency = frequencyFromMidiNote(msg.note)
-            time = msg.time
-            frequencyTimeVector = [frequency, time]
-            print(frequencyTimeVector)
-            frequencyTimeMatrix[i] = frequencyTimeVector
-            i += 1
-    '''
     return [[frequencyFromMidiNote(msg.note), msg.time] for msg in MidiFile(filename).tracks[0] if (msg.type == "note_on" and msg.time is not 0)  ]
 
 def getDuration(track):
