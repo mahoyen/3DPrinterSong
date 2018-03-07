e37fa38760aebda9eb623ef8a83424d54dfccaba
hermabe <32880010+hermabe@users.noreply.github.com>
2018-03-07 16:19:19 +0100

    Updated Epsilon, translateToCoordinates(), generateGCode()

--- MidiGcode/gcode.py
+++ MidiGcode/gcode.py
@@ -1,8 +1,8 @@
 # Constants
 FREQUENCYTOFEEDRATECONSTANT = 1
 TIMETODISTANCECONSTANT = 1
-BUILDING_AREA_MAX = [223, 223, 305] # [X, Y, Z]
-BUILDING_AREA_MIN = [10, 10, 10]
+BUILDING_AREA = [223, 223, 305] # [X, Y, Z]
+EPSILON = [10, 10, 10] # [X, Y, Z]
 STARTPOSITION = [100, 100, 100] # [X, Y, Z]
 
 # Global variables
@@ -12,18 +12,32 @@ direction = [1,1,1] # [X, Y, Z]
 def getFeedrateDistanceMatrix(frequencyTimeMatrix):
     return [[float(FREQUENCYTOFEEDRATECONSTANT*f), float(FREQUENCYTOFEEDRATECONSTANT*TIMETODISTANCECONSTANT*f/T)] for f, T in frequencyTimeMatrix]
 
-# Converts distances to coordinates
+# Converts distances to coordinates. returns list[list[list[x, y, z], feedrate]]
 def translateFeedrateDistanceMatrixToCoordinates(feedrateDistanceMatrix):
-    return [[relX, 0, 0] for feedrate, relX in feedrateDistanceMatrix]
+    coordinateList = {}
+    for feedrates, distance in feedrateDistanceMatrix:
+        absoluteFeedrate = sum([i**2 for i in feedrates])**0.5
+        duration = distance/absoluteFeedrate
+        coordinateList.append([[i*duration for i in feedrates], absoluteFeedrate])
+    
+    return coordinateList
+    # return [[relX, 0, 0] for feedrate, relX in feedrateDistanceMatrix]
 
 # Returns relative coordinate-vector from distance
-def translateDistanceToCoordinate(distance):
-    return [distance, 0, 0]
+# def translateDistanceToCoordinate(distance):
+#     return [distance, 0, 0]
+
+# Returns true if all notes in noteList are zero
+def isSilent(noteList):
+    for i in noteList:
+        if i:
+            return False
+    return True
 
 # Returns True if coordinate is outside build area
 def isCoordinateOutside(coordinate):
-    for i in coordinate:
-        if i < BUILDING_AREA_MIN or i > BUILDING_AREA_MAX:
+    for i in range(len(coordinate)):
+        if coordinate[i] < EPSILON[i] or coordinate[i] > BUILDING_AREA[i] - EPSILON[i]:
             return True
     return False
 
@@ -39,7 +53,7 @@ def calculateNewPosition(oldCoordinates, relCoordinates):
     for i in range(3):        
         newCoordinates[i] = oldCoordinates[i] + relCoordinates[i]*direction[i]
 
-        if (newCoordinates[i] < BUILDING_AREA_MIN[i] or newCoordinates[i] > BUILDING_AREA_MAX[i]):
+        if (newCoordinates[i] < EPSILON[i] or newCoordinates[i] > BUILDING_AREA[i] - EPSILON[i]):
             direction[i] *= -1
             newCoordinates[i] = oldCoordinates[i] + relCoordinates[i]*direction[i]
 
@@ -47,22 +61,9 @@ def calculateNewPosition(oldCoordinates, relCoordinates):
         raise ValueError("New position is outside of build area")            
     return newCoordinates
 
-#Takes in coordinatearray and feedrate and outputs gcode string
-def coordinatesToGCode_G0(newCoordinates, feedrate):
+#Takes in coordinatearray and feedrate and returns gcode string
+def coordinateToGCode_G0(newCoordinates, feedrate):
     return "G0 X"+str(newCoordinates[0])+" Y"+str(newCoordinates[1])+" Z"+str(newCoordinates[2])+" F"+str(feedrate)    
-'''
-def master(midifilename, gcodeFilename, startingCoordinates):
-    feedrateDistanceMatrix = getFeedrateDistanceMatrix(getFrequencyTimeMatrix(midifilename))
-    feedrateMatrix = [[feedrate] for feedrate, distanse in feedrateDistanceMatrix]
-    coordinates = translateFeedrateDistanceMatrixToCoordinates(feedrateDistanceMatrix)
-
-    for i in range(len(coordinates)):
-        if (not i):
-            oldCoordinates[i] = startingCoordinates
-        else:
-            
-            newPosition = calculateNewPosition(oldCoordinates[i], coordinates[i])
-'''
 
 # Returns a gcode string to pause for given amount of milliseconds
 def timeDelayToGCode_G4(milliSeconds):
@@ -74,7 +75,10 @@ def generateGCode(feedrateDistanceMatrix, filename):
         file.write(";FLAVOR:UltiGCode\n;TIME:346\n;MATERIAL:43616\n;MATERIAL2:0\n;NOZZLE_DIAMETER:0.4\nM82\n")        
         file.write(coordinatesToGCode_G0(STARTPOSITION, 3600) + "\n")
         file.write(timeDelayToGCode_G4(1000) + "\n")
-        coordinates = STARTPOSITION
-        for pair in feedrateDistanceMatrix:
-            coordinates = calculateNewPosition(coordinates, translateDistanceToCoordinate(pair[1]))
-            file.write(coordinatesToGCode_G0(coordinates, pair[0]) + "\n")      
+        coordinate = STARTPOSITION
+        for pair in translateFeedrateDistanceMatrixToCoordinates(feedrateDistanceMatrix):
+            if (isSilent(pair[0])):
+                file.write(timeDelayToGCode_G4(pair[1]))
+            else:
+                coordinate = calculateNewPosition(coordinate, pair[0])
+                file.write(coordinateToGCode_G0(coordinate, pair[1]) + "\n")
